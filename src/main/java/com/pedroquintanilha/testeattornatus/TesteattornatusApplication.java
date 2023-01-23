bcfef33b89ace9b320c9bac36c0a2a89027c5348
Pedro Quintanilha <pedrosquinta@gmail.com>
2023-01-23 17:56:41 -0300

    Iniciando

---
+++ src/main/java/com/pedroquintanilha/testeattornatus/TesteattornatusApplication.java
@@ -0,0 +1,13 @@
+package com.pedroquintanilha.testeattornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteattornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteattornatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroquintanilha/testeattornatus/TesteattornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroquintanilha.testeattornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteattornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
