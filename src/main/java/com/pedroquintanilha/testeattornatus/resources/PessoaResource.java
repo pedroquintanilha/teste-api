13cae4aecac73682d858c60d53bf789db27270b9
Pedro Quintanilha <pedrosquinta@gmail.com>
2023-01-24 01:16:11 -0300

    PessoaService e camada de Serviço

--- src/main/java/com/pedroquintanilha/testeattornatus/resources/PessoaResource.java
+++ src/main/java/com/pedroquintanilha/testeattornatus/resources/PessoaResource.java
@@ -1,24 +1,33 @@
 package com.pedroquintanilha.testeattornatus.resources;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pedroquintanilha.testeattornatus.entities.Pessoa;
+import com.pedroquintanilha.testeattornatus.services.PessoaService;
 
 @RestController
 @RequestMapping(value = "/pessoas")
 public class PessoaResource {
 
+	@Autowired
+	private PessoaService service;
+	
 	@GetMapping
-	public ResponseEntity<Pessoa> findAll() throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Pessoa p = new Pessoa(1L, "Pedro", sdf.parse("03/04/1994"));
-		return ResponseEntity.ok().body(p);
+	public ResponseEntity<List<Pessoa>> findAll() {
+		List<Pessoa> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Pessoa> findById(@PathVariable Long id) {
+		Pessoa obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/pedroquintanilha/testeattornatus/services/PessoaService.java
@@ -0,0 +1,26 @@
+package com.pedroquintanilha.testeattornatus.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedroquintanilha.testeattornatus.entities.Pessoa;
+import com.pedroquintanilha.testeattornatus.repositories.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository repository;
+	
+	public List<Pessoa> findAll() {
+		return repository.findAll();
+	}
+	
+	public Pessoa findById(Long id) {
+		Optional<Pessoa> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new IllegalArgumentException());
+	}
+}
