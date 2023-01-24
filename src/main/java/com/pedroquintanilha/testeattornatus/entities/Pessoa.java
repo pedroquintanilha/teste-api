16a95381fdd5987a77f5478bb43565dd938f1f8b
Pedro Quintanilha <pedrosquinta@gmail.com>
2023-01-23 21:22:36 -0300

    Banco de dados H2, perfil de teste

--- src/main/java/com/pedroquintanilha/testeattornatus/entities/Pessoa.java
+++ src/main/java/com/pedroquintanilha/testeattornatus/entities/Pessoa.java
@@ -3,11 +3,20 @@ package com.pedroquintanilha.testeattornatus.entities;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+
+@Entity
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	
